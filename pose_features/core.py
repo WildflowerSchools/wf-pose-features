d3f6f90863ba435a34b7b0367007add4bb727973
Theodore Quinn <tcquinn@tcquinn.org>
2023-10-07 09:27:02 -0500

    Implement function for imputing keypoints

--- pose_features/core.py
+++ pose_features/core.py
@@ -1,1 +1,24 @@
-# START CODE HERE
+import pandas as pd
+import numpy as np
+
+def impute_keypoints_pose_track(
+    pose_track,
+    timestamp_column_name='timestamp',
+    keypoint_coordinates_3d_column_name='keypoint_coordinates_3d',
+):
+    num_poses = len(pose_track)
+    pose_track_imputed = (
+        pose_track
+        .copy()
+        .sort_values('timestamp')
+    )
+    keypoint_coordinates_flattened = pd.DataFrame(
+        np.stack(pose_track_imputed[keypoint_coordinates_3d_column_name]).reshape((num_poses, -1)),
+        index=pose_track_imputed[timestamp_column_name]
+    )
+    keypoint_coordinates_flattened_interpolated = keypoint_coordinates_flattened.interpolate(method='time')
+    pose_track_imputed[keypoint_coordinates_3d_column_name] = list(
+        np.stack(keypoint_coordinates_flattened_interpolated.values)
+        .reshape((num_poses, -1, 3))
+    )
+    return pose_track_imputed
