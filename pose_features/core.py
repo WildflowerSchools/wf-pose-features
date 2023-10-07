891414a793e344f9de02132ae9ccbbd1db0da988
Theodore Quinn <tcquinn@tcquinn.org>
2023-10-07 09:53:45 -0500

    Implement method for computing shoulders center

--- pose_features/core.py
+++ pose_features/core.py
@@ -22,3 +22,40 @@ def impute_keypoints_pose_track(
         .reshape((num_poses, -1, 3))
     )
     return pose_track_imputed
+
+def remove_incomplete_poses(
+    poses,
+    keypoint_coordinates_3d_column_name='keypoint_coordinates_3d',
+):
+    poses_cleaned = (
+        poses
+        .loc[poses[keypoint_coordinates_3d_column_name].apply(lambda x: np.all(np.isfinite(x)))]
+        .copy()
+    )
+    return poses_cleaned
+
+def generate_shoulders_center_feature(
+    poses,
+    left_shoulder_index,
+    right_shoulder_index,
+    keypoint_coordinates_3d_column_name='keypoint_coordinates_3d',
+):
+    shoulders_center_feature = poses[keypoint_coordinates_3d_column_name].apply(
+        lambda pose: compute_shoulders_center(
+            pose=pose,
+            left_shoulder_index=left_shoulder_index,
+            right_shoulder_index=right_shoulder_index,
+        )
+    )
+    return shoulders_center_feature
+
+def compute_shoulders_center(
+    pose,
+    left_shoulder_index,
+    right_shoulder_index,
+):
+    shoulders_center = np.mean(
+        pose[[left_shoulder_index, right_shoulder_index], :],
+        axis=0
+    )
+    return shoulders_center
